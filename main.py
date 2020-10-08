a243dbfff057a9d2bb806233113a5b2d821bec68
will <williamspalding00@gmail.com>
2020-10-07 22:51:38 -0700

    added basic gym functionality

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("test123")
+import gym
+
+
+# create env
+env = gym.make('CartPole-v0')
+env.reset()
+
+# main environment loop
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+
+# close env
+env.close()
